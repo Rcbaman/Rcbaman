bb06f5f60f12647e7cfff5d53603baab9b63e2f4
Aman <rcubinityaman1203@gmail.com>
2021-08-25 16:57:58 +0530

    merge

--- database/migrations/2021_08_25_000003_create_product_crusts_table.php
+++ database/migrations/2021_08_25_000003_create_product_crusts_table.php
@@ -4,19 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-<<<<<<< HEAD:database/migrations/2021_08_25_000009_create_product_ingredients_table.php
-class CreateProductIngredientsTable extends Migration
-{
-    public function up()
-    {
-        Schema::create('product_ingredients', function (Blueprint $table) {
-=======
 class CreateProductCrustsTable extends Migration
 {
     public function up()
     {
         Schema::create('product_crusts', function (Blueprint $table) {
->>>>>>> origin/quickadminpanel_2021_08_25_11_18_41:database/migrations/2021_08_25_000003_create_product_crusts_table.php
             $table->bigIncrements('id');
             $table->timestamps();
             $table->softDeletes();
